b15981a92f8860c941f132b4ffbc5c80c8c28bdd
刘维军 <lwjsw2320214@live.cn>
2016-12-21 00:58:20 +0800

    添加自定义错误处理

--- src/main/java/com/gas/controller/UserController.java
+++ src/main/java/com/gas/controller/UserController.java
@@ -3,9 +3,13 @@ package com.gas.controller;
 import com.gas.entity.Result;
 import com.gas.entity.UserManage;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by GC on 2016/12/19.
@@ -16,15 +20,21 @@ public class UserController {
 
     @RequestMapping(value = "/register",method = RequestMethod.GET)
     @ResponseBody
-    public Object register(){
-        UserManage userManage=new UserManage();
-        userManage.setId("1111");
-        userManage.setUserName("username");
-        userManage.setLoginToken("1111111111111");
+    public Object register(Model model, HttpServletRequest request, HttpServletResponse response)  throws  Exception {
+
         Result  result=new Result();
-        result.setState(true);
-        result.setMessage("注册成功！");
-        result.setData(userManage);
+        int i=1;
+        if (i==2){
+            UserManage userManage=new UserManage();
+            userManage.setId("1111");
+            userManage.setUserName("username");
+            userManage.setLoginToken("1111111111111");
+            result.setState(true);
+            result.setMessage("注册成功！");
+            result.setData(userManage);
+        }else{
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        }
         return  result;
     }
 
